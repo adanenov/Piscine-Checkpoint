8708948bfe0e1dfed62edfdc890b0a64b0c434f6
adanenov <danenovabai06@gmail.com>
2026-02-05 21:46:27 +0000

    save

---
+++ digitlen_20/digitlen.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func DigitLen(n, base int) int {
+	if base < 2 || base > 32 {
+		return -1
+	}
+	if n < 0 {
+		n = -n
+	}
+	count := 0
+	for n > 0 {
+		n /= base
+		count++
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(DigitLen(100, 10))
+	fmt.Println(DigitLen(100, 2))
+	fmt.Println(DigitLen(-100, 16))
+	fmt.Println(DigitLen(100, -1))
+}
